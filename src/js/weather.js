6a1acf77a757cc7a731aa603e590b4315f06f972
nvctuan2302 <nvctuan2302@gmail.com>
2022-01-10 19:18:51 +0700

    fix error 404

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,3 +1,4 @@
+import { data } from "browserslist";
 
 const nameCity = document.querySelector(".js-weather-current__name");
 const descriptionWeather = document.querySelector(".js-weather-current__desc");
@@ -7,34 +8,66 @@ const iconCurrentWeather = document.querySelector(".js-weather-current__icon");
 const refreshCurrentWeather = document.querySelector(".js-weather-current__redo");
 const getSearchInput = document.querySelector(".js-search__input");
 const getSearchBtn = document.querySelector(".js-search__btn");
-let timeCurrentWeather = document.querySelector(".js-weather-current__time");
-
-
-
+const humidityCurrentWeather = document.querySelector(".js-expand-list__humidity");
+const visibilityCurrentWeather = document.querySelector(".js-expand-list__visibility");
+const pressureCurrentWeather = document.querySelector(".js-expand-list__pressure");
+const windCurrentWeather = document.querySelector(".js-expand-list__wind");
+const uviCurrentWeather = document.querySelector(".js-expand-list__uvi");
+const dewpointCurrentWeather = document.querySelector(".js-expand-list__dewpoint");
 const api = {
   key: "2f887e5d1ea0b70b225c193184f78cd2",
   baseUrl: "https://api.openweathermap.org/data/2.5/",
 };
+let timeCurrentWeather = document.querySelector(".js-weather-current__time");
 
-const status = (response) => {
-  if (
-    (response.status >= 200 && response.status < 300) ||
-    response.status === 404
-  )
-    return response.json();
-};
 
-const displayWeatherCurrent = (data) => {
-  const { main, description, icon } = data.weather[0];
-  const { temp, feels_like } = data.main;
+const displayWeatherCurrent = data => {
+  const { description, icon } = data.current.weather[0];
+  const { temp,
+    feels_like,
+    humidity,
+    pressure,
+    wind_speed,
+    visibility,
+    uvi,
+    dew_point } = data.current;
 
-  nameCity.innerText = `${data.name}, ${data.sys.country}`;
   descriptionWeather.innerText = description;
-  tempCurrentWeather.innerText = temp;
-  feelsCurrentWeather.innerText = feels_like;
+  tempCurrentWeather.innerText = Math.round(temp);
+  uviCurrentWeather.innerText = uvi;
+  feelsCurrentWeather.innerText = Math.round(feels_like);
+  humidityCurrentWeather.innerText = humidity;
+  dewpointCurrentWeather.innerText = dew_point;
+  pressureCurrentWeather.innerText = pressure;
+  windCurrentWeather.innerText = wind_speed;
+  visibilityCurrentWeather.innerText = (visibility / 1000);
   iconCurrentWeather.style.backgroundImage = `url('http://openweathermap.org/img/wn/${icon}@4x.png')`;
 };
 
+const status = (response) => {
+  if ((response.status >= 200 && response.status < 300) || response.status === 404) {
+    return response.json();
+  }
+};
+
+const error = error => {
+  alert(`Không tìm thấy thành phố !!!!`, error);
+}
+
+
+const getData = url => {
+  fetch(`${url}`)
+    .then(status)
+    .then(data => {
+      nameCity.innerText = `${data.name},${data.sys.country}`;
+
+      fetch(`${api.baseUrl}onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,alerts&units=metric&appid=${api.key}&lang=vi`)
+        .then(status)
+        .then(displayWeatherCurrent)
+    }).catch(error)
+
+
+}
 
 setInterval((_) => {
   let date = new Date();
@@ -44,28 +77,19 @@ setInterval((_) => {
   let hours = `${date.getHours()}`;
   let minutes = `0${date.getMinutes()}`;
   let seconds = `0${date.getSeconds()}`;
-  timeCurrentWeather.innerText = `T6 ${day}/${month} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+  timeCurrentWeather.innerText = `T2 ${day}/${month} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
 }, 1000);
 
-const fetchApi = (url) => {
-  fetch(`${url}`)
-    .then(status)
-    .then(displayWeatherCurrent)
-    .catch((error) => {
-      alert("Khồng tìm thấy thành phố !!!!");
-    });
-};
 
 const getCurrentPosition = (position) => {
   const { latitude, longitude } = position.coords;
 
-  fetchApi(`${api.baseUrl}weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api.key}&lang=vi`);
+  getData(`${api.baseUrl}weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api.key}&lang=vi`);
 };
 
 const getResults = (query) => {
-  fetchApi(`${api.baseUrl}weather?q=${query}&units=metric&appid=${api.key}&lang=vi`);
+  getData(`${api.baseUrl}weather?q=${query}&units=metric&appid=${api.key}&lang=vi`);
 };
-
 export const weather = _ => {
   navigator.geolocation.getCurrentPosition(getCurrentPosition);
 
@@ -73,17 +97,17 @@ export const weather = _ => {
     if (e.keyCode === 13 && e.target.value !== "") {
       getResults(e.target.value);
       e.target.value = "";
-    }
+    };
   });
 
   getSearchBtn.addEventListener("click", e => {
     if (getSearchInput.value !== "") {
       getResults(getSearchInput.value)
       getSearchInput.value = "";
-    }
-  })
+    };
+  });
 
   refreshCurrentWeather.addEventListener("click", _ => {
     getResults(nameCity.innerText);
-  })
+  });
 };
