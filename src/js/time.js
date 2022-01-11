4117e35864f07f94a97674cbaa1451035604e9d6
nvctuan2302 <nvctuan2302@gmail.com>
2022-01-12 00:27:10 +0700

    fix bug : Epoch & Unix Timestamp Conversion Tools

--- src/js/time.js
+++ src/js/time.js
@@ -1,14 +1,29 @@
 let timeNow = document.querySelector(".js-weather-current__time");
 
-export const time = _ => {
-  setInterval(_ => {
-    let date = new Date();
+const timeConverter = (UNIX_timestamp) => {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  let arrDay = ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = date + ' ' + month + ' ' + year ;
+  return time;
+}
 
-    let day = `${date.getDate()}`;
-    let month = `${date.getMonth()}` + 1;
-    let hours = `${date.getHours()}`;
-    let minutes = `0${date.getMinutes()}`;
-    let seconds = `0${date.getSeconds()}`;
-    timeNow.innerText = `T2 ${day}/${month} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
-  }, 1000);
+const realTime = _ => {
+  let d = new Date();
+  let arrDay = ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'];
+
+  let day = d.getDay();
+  let date = d.getDate();
+  let month = `${d.getMonth()}` + 1;
+  let hours = `${d.getHours()}`;
+  let minutes = `0${d.getMinutes()}`;
+  let seconds = `0${d.getSeconds()}`;
+  timeNow.innerText = `${arrDay[day]} ${date}/${month} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+}
+
+export const time = _ => {
+  setInterval(realTime(), 1000);
 }
